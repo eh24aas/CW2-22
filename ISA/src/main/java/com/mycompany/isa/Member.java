e8500ceed71ec6ece23fb1571b284724156894b3
HarryChung52 <hc23abi@herts.ac.uk>
2026-03-17 14:55:47 +0000

    added some function in member

--- ISA/src/main/java/com/mycompany/isa/Member.java
+++ ISA/src/main/java/com/mycompany/isa/Member.java
@@ -5,10 +5,58 @@
  */
 package com.mycompany.isa;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author elean
  */
 public class Member {
+    private String name;
+    private String address;
+    private String email;
+    private int donatedQty;
+    private ArrayList<Item> borrowing;
     
+    public Member(String name, String address, String email){
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public int getDonatedQty(){
+        return donatedQty;
+    }
+    
+    public int borrowingQty(){
+        return borrowing.size();
+    }
+    
+    public ArrayList<Item> getLoanItem(){
+        return borrowing;
+    }
 }
