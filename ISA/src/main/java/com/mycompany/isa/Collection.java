4813368740bd301062c3df305bd03b17ff236287
HarryChung52 <hc23abi@herts.ac.uk>
2026-03-17 15:54:31 +0000

    collection change

--- ISA/src/main/java/com/mycompany/isa/Collection.java
+++ ISA/src/main/java/com/mycompany/isa/Collection.java
@@ -4,10 +4,20 @@
  */
 package com.mycompany.isa;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author elean
  */
 public class Collection {
+    private ArrayList<Item> Items;
     
+    public Collection(){
+        Items = new ArrayList<>();
+    }
+    
+    public void addBook(String title, String author, Member donateor, String language, String isbn){
+        Item book = new Item(title, author);
+    }
 }
