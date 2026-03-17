68c3af04a7c7bdb19840baf3a27e709ac0876c44
HarryChung52 <hc23abi@herts.ac.uk>
2026-03-17 01:22:03 +0000

    I finish the book script, and I add the function getLanguage in Item.

--- ISA/src/main/java/com/mycompany/isa/Book.java
+++ ISA/src/main/java/com/mycompany/isa/Book.java
@@ -13,4 +13,21 @@ public class Book extends Item{
     private String author;
     private String isbn;
     
+    public Book(String title, String arthur, Member donatedBy, String language, String isbn){
+        super(title, language, donatedBy);
+        this.author = author;
+        this.isbn = isbn;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+    
+    public String toString(){
+        return "Book |" + getTitle() + "|" + author + "|" + isbn + "|" + getLanguage() + "|" + getDonator().getEmail() ;
+    }
 }
