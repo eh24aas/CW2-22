fbd40eaa6cb111e83f3823afe4ed8e51a1f7d37e
HarryChung52 <hc23abi@herts.ac.uk>
2026-03-17 15:05:02 +0000

    DVD Class: added getters, setters, toString

--- ISA/src/main/java/com/mycompany/isa/DVD.java
+++ ISA/src/main/java/com/mycompany/isa/DVD.java
@@ -14,11 +14,22 @@ public class DVD extends Item{
     private String director;
     private String[] audioLanguages;
     
-    public DVD (String title, String director, Member donatedBy, String Language, String[] audioLanguages) {
-        super(language, title, donatedBy);
-        this.audioLanguages = audioLanguages[];
+    public DVD (String title, String director, Member donatedBy, String language, String[] audioLanguages) {
+        super(title, language, donatedBy);
+        this.audioLanguages = audioLanguages;
         this.director = director;
     }
    
+    public void setDirector(String director){
+        this.director = director;
+    }
+    
+    public void setAudioLanguages(String[] languages){
+        this.audioLanguages = languages;
+    }
+    
+    public String toString(){
+        return "DVD|" + super.getTitle() + "|" + super.getLanguage() + "|" + director + "|" + audioLanguages + "|" + super.getDonator().getEmail();
+    }
     
 }
