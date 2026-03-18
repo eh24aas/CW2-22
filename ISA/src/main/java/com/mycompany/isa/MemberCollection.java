ae52e041884cfadfecf8adbb40234c1fac3762f7
eh24aas <eh24aas@herts.ac.uk>
2026-03-18 00:04:52 +0000

    MemberCollection Class: added attributes

---
+++ ISA/src/main/java/com/mycompany/isa/MemberCollection.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.isa;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author elean
+ */
+public class MemberCollection {
+    
+    private ArrayList<Member> members;
+    
+    
+}
