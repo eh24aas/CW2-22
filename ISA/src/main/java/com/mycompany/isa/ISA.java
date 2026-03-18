4986efa41ca9f98c6abe8eb173e93501ea73abaf
eh24aas <eh24aas@herts.ac.uk>
2026-03-18 23:35:52 +0000

    added items file and tested printing line-by line

--- ISA/src/main/java/com/mycompany/isa/ISA.java
+++ ISA/src/main/java/com/mycompany/isa/ISA.java
@@ -4,6 +4,13 @@
 
 package com.mycompany.isa;
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
+
+
+
 /**
  *
  * @author elean
@@ -12,5 +19,31 @@ public class ISA {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        
+        try (Scanner scanner  = new Scanner(new File("input-1.dat"))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        }   catch (IOException o){
+            System.out.println("file not found");
+        }
+        
+        
+        //testing 
+        
+        Member m1 = new Member("name1","add1","e1");
+        Member m2 = new Member("name2","add2","e2");
+        
+        MemberCollection collection = new MemberCollection();
+        
+        collection.addMember("john cena","64 zoo lane","js@gmail.com");
+        
+        for (Member member: collection.getMembers()){
+            System.out.println(member.getName());
+        }
+        
+        
+        
     }
 }
